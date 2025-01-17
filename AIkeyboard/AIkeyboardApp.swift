65b9fa2865b1bedea4edc27840eefc2a1065e9a8
Reign Campbell <raine@businessbuddy.sg>
2025-01-17 21:32:36 +0800

    Initial Commit

---
+++ AIkeyboard/AIkeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIkeyboardApp.swift
+//  AIkeyboard
+//
+//  Created by Raine Goh on 17/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AIkeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIkeyboardTests/AIkeyboardTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIkeyboardTests.swift
+//  AIkeyboardTests
+//
+//  Created by Raine Goh on 17/1/25.
+//
+
+import Testing
+
+struct AIkeyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
